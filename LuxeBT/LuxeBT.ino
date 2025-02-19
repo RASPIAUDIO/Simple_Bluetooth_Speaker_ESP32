c426be26b18ca5e18056e7b6c297d75ca7dcfb3a
RASPIAUDIOadmin <olivier@ros.fr>
2025-02-19 11:23:37 +0100

    02/19/25 update

--- LuxeBT/LuxeBT.ino
+++ LuxeBT/LuxeBT.ino
@@ -11,10 +11,11 @@
 #include "driver/gpio.h"
 #include "esp_mac.h"
 #include <Adafruit_NeoPixel.h>
+#include "LittleFS.h"
 
 
 bool BPause = false;
-int volume = 60;                            // 0...100
+int volume = 75;                            // 0...100
 #define maxVol 100
 // Initialize your objects and variables
 I2SClass i2s;
@@ -26,10 +27,24 @@ char macStr[20];
 char dev_name [30];
 bool jack = false;
 ES8388 es;
-
+uint8_t *b;
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+
+
 void setup() {
   Serial.begin(115200);
+
+  // Initialisation du TWDT avec votre configuration personnalisée
+  esp_task_wdt_config_t wdt_config = {
+    .timeout_ms = 60 * 1000,                // 60 secondes en millisecondes
+    .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,  // Surveille tous les cœurs
+    .trigger_panic = true                  // Active le mode panic en cas de timeout
+  };
+  esp_task_wdt_init(&wdt_config);
+
+
+
 ////// GPIOs init  
   // power enable
   gpio_reset_pin(GPIO_PA_EN);
@@ -57,145 +72,165 @@ void setup() {
   gpio_set_direction(Jack_Detect, GPIO_MODE_INPUT);
   gpio_set_pull_mode(Jack_Detect, GPIO_PULLUP_ONLY);  
 
-  
-  
-//////I2S init
+
+
+  // Boutons, I2S...
   i2s.setPins(I2S_BCLK, I2S_LRCK, I2S_SDOUT, I2S_SDIN, I2S_MCLK);
-  if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
+  if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT,
+                 I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
     Serial.println("Failed to initialize I2S!");
-    while (1); // do nothing
+    while (1);
   }
 
+  // Initialisation du codec ES8388
   Serial.printf("Connect to ES8388 codec... ");
-  while (not es.begin(IIC_DATA, IIC_CLK))
-  {
-      Serial.printf("Failed!\n");
-      delay(1000);
+  while (!es.begin(IIC_DATA, IIC_CLK)) {
+    Serial.printf("Failed!\n");
+    delay(1000);
   }
-  Serial.printf("OK\n");
+  
+  
+  
+  Serial.println("OK");
   es.volume(ES8388::ES_MAIN, maxVol);
   es.volume(ES8388::ES_OUT1, volume);
-//    es.volume(ES8388::ES_OUT2, volume);
-  es.mute(ES8388::ES_OUT1, false);
-//    es.mute(ES8388::ES_OUT2, false);
   es.mute(ES8388::ES_MAIN, false);
+  es.mute(ES8388::ES_OUT1, false);
 
-////// Specific BT device name (using  MAC ref)
-
-  esp_read_mac((uint8_t*)&mac, ESP_MAC_BT);
-  snprintf(macStr, 19, "-%x%x%x", mac[3], mac[4], mac[5]);
+  // Construction du nom Bluetooth
+  esp_read_mac(mac, ESP_MAC_BT);
+  snprintf(macStr, sizeof(macStr), "-%x%x%x", mac[3], mac[4], mac[5]);
   strcpy(dev_name, "LUXE");
   strcat(dev_name, macStr);
-  printf("Device name : %s\n", dev_name);
-  
-////// Bluetooth A2DP sink init
+  Serial.print("Device name : ");
+  Serial.println(dev_name);
+
+  // Démarrage du Bluetooth A2DP Sink
   a2dp_sink.start(dev_name);
   Serial.println("Bluetooth A2DP Sink Initialized");
 
-/////task managing the battery
-  xTaskCreatePinnedToCore(battery, "battery", 5000, NULL, 5, NULL,0);  
-  
+  // Initialisation de LittleFS et lecture du fichier WAV, etc.
+  if (!LittleFS.begin()) {
+    Serial.println("LittleFS initialisation failed!");
+    while (1) for (;;);
+  }
+
+  b = (uint8_t *)ps_malloc(160000);
+  File ln = LittleFS.open("/bluetooth.wav", FILE_READ);
+  ln.read(b, 115000);
+  ln.close();
+  i2s.playWAV(b, 115000);
+  free(b);
+
+  i2s.end();
+  i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH);
+
+  // Lancement de la tâche de monitoring de la batterie
+  xTaskCreatePinnedToCore(battery, "battery", 4096, NULL, 5, NULL, 0);
 }
 
 void loop() {
+  printf("1\n");
   if (a2dp_sink.get_audio_state() != ESP_A2D_AUDIO_STATE_STARTED) delay(1000);
-
-/////// Play/Pause button (using AVRC)  
+  printf("2\n");
+  /////// Play/Pause button (using AVRC)
+  
   if (gpio_get_level(BUTTON_PAUSE) == 0)
   {
-    while(gpio_get_level(BUTTON_PAUSE) == 0) delay(10);
-    
+      printf("5\n");
+    while (gpio_get_level(BUTTON_PAUSE) == 0) delay(10);
+  printf("3\n");
     if (BPause == false)
-    {  
-      es.mute(ES8388::ES_MAIN,true);      
-      if(jack) 
+    {
+      es.mute(ES8388::ES_MAIN, true);
+      if (jack)
       {
-        es.volume(ES8388::ES_OUT2, 0);  
+        es.volume(ES8388::ES_OUT2, 0);
         es.mute(ES8388::ES_OUT2, true);
       }
-      else 
+      else
       {
-      es.volume(ES8388::ES_OUT1, 0); 
-      es.mute(ES8388::ES_OUT1, true);     
+        es.volume(ES8388::ES_OUT1, 0);
+        es.mute(ES8388::ES_OUT1, true);
       }
       a2dp_sink.pause();
       BPause = true;
     }
     else
     {
-      es.mute(ES8388::ES_MAIN,false);
-      if(jack) 
+      es.mute(ES8388::ES_MAIN, false);
+      if (jack)
       {
-      es.volume(ES8388::ES_OUT2, volume);  
-      es.mute(ES8388::ES_OUT2, false);      
+        es.volume(ES8388::ES_OUT2, volume);
+        es.mute(ES8388::ES_OUT2, false);
       }
-      else 
+      else
       {
-      es.volume(ES8388::ES_OUT1, volume);   
-      es.mute(ES8388::ES_OUT1, false);
+        es.volume(ES8388::ES_OUT1, volume);
+        es.mute(ES8388::ES_OUT1, false);
       }
       a2dp_sink.play();
       BPause = false;
     }
   }
-
-////// Vol+ button  
+  
+  printf("4\n");
+  ////// Vol+ button
   if (gpio_get_level(BUTTON_VOL_PLUS) == 0)
   {
-    while(gpio_get_level(BUTTON_VOL_PLUS) == 0) delay(10);
+    while (gpio_get_level(BUTTON_VOL_PLUS) == 0) delay(10);
     volume += 3;
-    if(volume > maxVol) volume = maxVol;
+    if (volume > maxVol) volume = maxVol;
     printf(" v = %d\n", volume);
-    if(volume < 6)es.volume(ES8388::ES_MAIN, volume); else es.volume(ES8388::ES_MAIN, maxVol);    
-    if(jack)   
-    es.volume(ES8388::ES_OUT2, volume); 
+    if (volume < 6)es.volume(ES8388::ES_MAIN, volume); else es.volume(ES8388::ES_MAIN, maxVol);
+    if (jack)
+      es.volume(ES8388::ES_OUT2, volume);
     else
-    es.volume(ES8388::ES_OUT1, volume); 
+      es.volume(ES8388::ES_OUT1, volume);
   }
 
-////// Vol- button  
+  ////// Vol- button
   if (gpio_get_level(BUTTON_VOL_MINUS) == 0)
   {
-    while(gpio_get_level(BUTTON_VOL_MINUS) == 0) delay(10);    
+    while (gpio_get_level(BUTTON_VOL_MINUS) == 0) delay(10);
     volume -= 3;
-    if(volume < 0) volume = 0;
-    printf(" v = %d\n", volume);   
-    if(volume < 6)es.volume(ES8388::ES_MAIN, volume); else es.volume(ES8388::ES_MAIN, maxVol);          
-    if(jack)   
-    es.volume(ES8388::ES_OUT2, volume); 
+    if (volume < 0) volume = 0;
+    printf(" v = %d\n", volume);
+    if (volume < 6)es.volume(ES8388::ES_MAIN, volume); else es.volume(ES8388::ES_MAIN, maxVol);
+    if (jack)
+      es.volume(ES8388::ES_OUT2, volume);
     else
-    es.volume(ES8388::ES_OUT1, volume);     
+      es.volume(ES8388::ES_OUT1, volume);
   }
 
-  
-///// Jack_Detect
-  if(gpio_get_level(Jack_Detect) == 0)
+
+  ///// Jack_Detect
+  if (gpio_get_level(Jack_Detect) == 0)
   {
-// jack ON => Rout2/Lout2  amp OFF 
-    if(!jack)
+    // jack ON => Rout2/Lout2  amp OFF
+    if (!jack)
     {
-    es.volume(ES8388::ES_OUT2, volume); 
-    es.select_out2(); 
-    gpio_set_level(GPIO_PA_EN, 0); 
-    }    
-    jack = true;       
+      es.volume(ES8388::ES_OUT2, volume);
+      es.select_out2();
+      gpio_set_level(GPIO_PA_EN, 0);
+    }
+    jack = true;
   }
   else
   {
-// jack OFF => Rout1/Lout1  amp ON  
-    if(jack) 
+    // jack OFF => Rout1/Lout1  amp ON
+    if (jack)
     {
-    es.volume(ES8388::ES_OUT1, volume);
-    es.select_out1();    
-    gpio_set_level(GPIO_PA_EN, 1); 
-    }    
-    jack = false;     
+      es.volume(ES8388::ES_OUT1, volume);
+      es.select_out1();
+      gpio_set_level(GPIO_PA_EN, 1);
+    }
+    jack = false;
   }
+//  vTaskDelay(100 / portTICK_PERIOD_MS);
+}
 
-    vTaskDelay(100/portTICK_PERIOD_MS);
-  }
 
-  
 //////////////////////////////////////////////////////////////////////////
 // task for battery monitoring
 //////////////////////////////////////////////////////////////////////////
@@ -208,15 +243,16 @@ void loop() {
 static void battery(void* pdata)
 {
   int val;
-  while(1)
+  while (1)
   {
-   val = analogRead(ADC_battery);
-//   printf("Battery : %d\n");
-   if(val < NYELLOW) pixels.setPixelColor(0, pixels.RED);       //RED
-   else if(val > NGREEN) pixels.setPixelColor(0, pixels.GREEN); //GREEN
-   else pixels.setPixelColor(0, pixels.YELLOW);                 //YELLOW
-   pixels.show();   
-   vTaskDelay(10000/portTICK_PERIOD_MS);
+    val = analogRead(ADC_battery);
+    printf("Battery : %d\n");
+    if (val < NYELLOW) pixels.setPixelColor(0, pixels.RED);      //RED
+    else if (val > NGREEN) pixels.setPixelColor(0, pixels.GREEN); //GREEN
+    else pixels.setPixelColor(0, pixels.YELLOW);                 //YELLOW
+    pixels.show();
+    taskYIELD(); // Cède la main aux autres tâches
+
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
   }
 }
-  
